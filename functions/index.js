bcec48037796e6bec0dd4ecbc9810316f664b08c
Saba-Shavidze <saba@ancient.gg>
2023-02-02 20:54:39 +0400

    save

--- functions/index.js
+++ functions/index.js
@@ -12,13 +12,13 @@ var _app = _interopRequireDefault(require("./src/app"));
 var _express = _interopRequireDefault(require("express"));
 var _path = _interopRequireDefault(require("path"));
 var _fs = _interopRequireDefault(require("fs"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-var app = (0, _express["default"])();
-var index = _fs["default"].readFileSync(_path["default"].resolve(__dirname, "..") + "/public/index.html", "utf-8");
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+var app = (0, _express.default)();
+var index = _fs.default.readFileSync(_path.default.resolve(__dirname, "..") + "/public/index.html", "utf-8");
 app.use("^/$", function (req, res, next) {
-  var html = "".concat((0, _server.renderToString)( /*#__PURE__*/_react["default"].createElement(_app["default"], {
+  var html = "".concat((0, _server.renderToString)( /*#__PURE__*/_react.default.createElement(_app.default, {
     initialValue: 10
   })));
   var replacedHTML = index.replace("<div id=\"root\"></div>", "<div id=\"root\">".concat(html, "</div>"));
--- functions/src/index.js
+++ functions/src/index.js
@@ -3,5 +3,5 @@
 var _react = _interopRequireDefault(require("react"));
 var _client = require("react-dom/client");
 var _app = _interopRequireDefault(require("./app"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-(0, _client.hydrateRoot)(document.getElementById("root"), /*#__PURE__*/_react["default"].createElement(_app["default"], null));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+(0, _client.hydrateRoot)(document.getElementById("root"), /*#__PURE__*/_react.default.createElement(_app.default, null));
